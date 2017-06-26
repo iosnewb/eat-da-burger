53d54e38388cd45cfb19589c73bb4b0f2e0c631b
Tri Ton <triton@MacBook-Pro.local>
2017-06-25 19:25:12 -0700

    updates

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,19 @@
 var mysql = require('mysql');
 
-// MySQL DB Connection Information (remember to change this with our specific credentials)
-var connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "burgers_db"
-});
+var connection;
 
-// Initiate MySQL Connection.
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
+if(process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'burgers_db'
+  });
+};
+
+connection.connect();
 
 module.exports = connection;
 
